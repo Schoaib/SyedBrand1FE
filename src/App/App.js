c0d7736e5e6714f34113a86a9a289319b99a95d0
Syed Shoaib <syed@viafone.com>
2017-08-05 02:32:30 +0400

    v2

--- src/App/App.js
+++ src/App/App.js
@@ -2,22 +2,19 @@
 import React, {Component} from 'react';
 import logo from './images/logo_en_ae.svg';
 import './styles.css';
-import {Catalog} from 'syedcommonfe';
+import Catalog from '../Components/Catalog';
 
-const BACKEND_API = "http://localhost:8080/mamas&papas";
+const BACKEND_API = "http://localhost:8080";
 const PRODUCT_IMAGE_PATH = "//prod2.mnpcdn.ae/small_light(p=listing2x,of=webp)/pub/media/catalog/product/";
 
-
 class App extends Component {
-
-
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
         </div>
-        <Catalog backendApi={BACKEND_API} productImagePath={PRODUCT_IMAGE_PATH}></Catalog>
+        <Catalog backendApi={BACKEND_API} productImagePath={PRODUCT_IMAGE_PATH} match={this.props.match}></Catalog>
       </div>
     );
   }
